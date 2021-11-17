31b3ed6e75cde6a4e7888213a4b94252105ee605
Egor <glavnegor@yandex.ru>
2021-11-17 12:47:39 +0300

    Task1 completed

---
+++ src/skypro/java/course1/lesson2/Task1.java
@@ -0,0 +1,16 @@
+package skypro.java.course1.lesson2;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        byte regionNumber = 78;
+        short totalCoronaInfectionsHaiti = 24_710;
+        int distanceEarthToMoon = 384_000;
+        long solarSystemAge = 4_570_000_000L;
+        float a = 6.0223f;
+        double e = 2.7182818284;
+        char aUppercase = 'A';
+        boolean b = distanceEarthToMoon > totalCoronaInfectionsHaiti;
+
+    }
+}
