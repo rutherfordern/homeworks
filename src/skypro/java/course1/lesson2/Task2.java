e8948897038462bda10138c83aa5d2c12839d382
Egor <glavnegor@yandex.ru>
2021-11-17 13:18:43 +0300

    Task2 completed

---
+++ src/skypro/java/course1/lesson2/Task2.java
@@ -0,0 +1,16 @@
+package skypro.java.course1.lesson2;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        float weightFirstBoxer = 78.2f;
+        float weightSecondBoxer = 82.7f;
+
+        float totalWeightBoxer = weightFirstBoxer + weightSecondBoxer;
+        float differenceWeightBoxer = weightSecondBoxer - weightFirstBoxer;
+
+        System.out.println(totalWeightBoxer);
+        System.out.println(differenceWeightBoxer);
+
+    }
+}
