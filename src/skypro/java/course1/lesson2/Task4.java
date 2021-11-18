8d2b7f006e98bf1083361330f278a7ab6b6d5f28
Egor <glavnegor@yandex.ru>
2021-11-18 11:00:44 +0300

    внес правки

--- src/skypro/java/course1/lesson2/Task4.java
+++ src/skypro/java/course1/lesson2/Task4.java
@@ -3,13 +3,13 @@ package skypro.java.course1.lesson2;
 public class Task4 {
     public static void main(String[] args) {
 
-        int weightDesired = 7000; // вес в граммах, который необходимо сбросить
+        int weightToLoose = 7000; // вес в граммах, который необходимо сбросить
         int weightLostPerDay1 = 250; // грамм необходимо сбрасывать в день
         int weightLostPerDay2 = 500; // грамм необходимо сбрасывать в день
 
         // Подсчет кол-ва дней для похудения
-        int totalDays1 = weightDesired / weightLostPerDay1;
-        int totalDays2 = weightDesired / weightLostPerDay2;
+        int totalDays1 = weightToLoose / weightLostPerDay1;
+        int totalDays2 = weightToLoose / weightLostPerDay2;
 
         System.out.println(totalDays1 + " дней потребуется при похудении на 7 кг, если сбрасывать 250 грамм.");
         System.out.println(totalDays2 + " дней потребуется при похудении на 7 кг, если сбрасывать 500 грамм.");
