2ab68b9b788110f91ce371a40e43216d77c22c98
Egor <glavnegor@yandex.ru>
2021-11-29 12:59:32 +0300

    Задача 1 выполнена

---
+++ src/skypro/java/course1/lesson5/TasksLesson.java
@@ -0,0 +1,17 @@
+package skypro.java.course1.lesson5;
+
+public class TasksLesson {
+    public static void main(String[] args) {
+
+        // Задача 1
+        int[] intArray = new int[3];
+        intArray[0] = 1;
+        intArray[1] = 2;
+        intArray[2] = 3;
+
+        double[] doubleArray = {1.57, 7.654, 9.986};
+
+        String[] stringArray = {"hello", "world", "xe-xe"};
+
+    }
+}
