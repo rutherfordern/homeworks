45714352c44d1db3f1e52d554cb0efa2f6ea38f6
Egor <glavnegor@yandex.ru>
2021-12-06 13:57:00 +0300

    Выполнил задачу 1

---
+++ src/skypro/java/course1/lesson7/TasksLesson.java
@@ -0,0 +1,15 @@
+package skypro.java.course1.lesson7;
+
+public class TasksLesson {
+    public static void main(String[] args) {
+
+        // Задача 1
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+    }
+}
