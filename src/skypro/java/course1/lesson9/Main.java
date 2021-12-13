df60eed97a37de87e4b60282da9bfeaee6935438
Egor <glavnegor@yandex.ru>
2021-12-13 18:20:35 +0300

    Выполнил задачи hard уровня

--- src/skypro/java/course1/lesson9/Main.java
+++ src/skypro/java/course1/lesson9/Main.java
@@ -12,32 +12,23 @@ public class Main {
 
         book2.setBookPublicationYear(2019);
 
-        Book[] books = new Book[2];
-        addBook(books, book1);
-        addBook(books, book2);
+        Library library = new Library(2);
 
-        printBooks(books);
+        library.addBook(book1);
+        library.addBook(book2);
+
+        library.printAllBooks();
+        library.printNameBooks("Science and method");
+
+        library.changePublicationYear("Math with Bad Drawings", 2018);
+
+
+
+
+
 
     }
 
-    public static void addBook(Book[] books, Book book) {
-        for (int i = 0; i < books.length; i++) {
-            if (books[i] == null) {
-                books[i] = book;
-                return;
-            }
-
-        }
-    }
-
-    public static void printBooks(Book[] books) {
-        for (Book book : books) {
-            if (book != null) {
-                System.out.println(book.getAuthor().getFirstName() + " " + book.getAuthor().getLastName()
-                + ": " + book.getNameBook() + ": " + book.getPublicationYear());
-            }
-        }
-    }
 
 }
 
